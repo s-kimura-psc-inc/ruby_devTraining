1aa85f51a276ddf6e9b63abe3d02b5e42280905d
s-kimura <s-kimura@psc-inc.co.jp>
2018-04-16 10:51:14 +0900

    seedsファイル修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,16 @@
 # ユーザ
-
-SYSTEM_USER = 'system'.freeze
-
 ApplicationRecord.connection.execute("TRUNCATE #{User.table_name}")
-User.create(login: "user-1", password: 'password', password_confirmation: 'password', user_nm: "ユーザー1", authority_level: Constants::AUTHORITY_LEVEL_GENERAL[:value], created_by: SYSTEM_USER, updated_by: SYSTEM_USER)
-User.create(login: "admin-1", password: 'password', password_confirmation: 'password', user_nm: "システム管理者1", authority_level: Constants::AUTHORITY_LEVEL_ADMIN[:value], created_by: SYSTEM_USER, updated_by: SYSTEM_USER)
+User.create(login: "user-1", password: 'password', password_confirmation: 'password', user_nm: 'ユーザー1', authority_level: 1, created_by: 'system', updated_by: 'system')
+User.create(login: "admin-1", password: 'password', password_confirmation: 'password', user_nm: 'システム管理者1', authority_level: 2, created_by: 'system', updated_by: 'system')
 
+# ステータスマスタ
+ApplicationRecord.connection.execute("TRUNCATE #{Status.table_name}")
+CSV.foreach(Rails.root.join('db/seed/mst_statuses.csv'), headers: true, skip_blanks: true) do |row|
+  Status.create(status_cd: row[0], status_nm: row[1])
+end
+
+# 優先度マスタ
+ApplicationRecord.connection.execute("TRUNCATE #{Priority.table_name}")
+CSV.foreach(Rails.root.join('db/seed/mst_priorities.csv'), headers: true, skip_blanks: true) do |row|
+  Priority.create(priority_cd: row[0], priority_nm: row[1])
+end
